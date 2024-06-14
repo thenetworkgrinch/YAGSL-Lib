3373a4b352b62507892e80b602f11013e90ee134
thenetworkgrinch <thenetworkgrinch@users.noreply.github.com>
2024-06-14 12:58:14 -0500

    Consistency

--- src/main/java/swervelib/SwerveDrive.java
+++ src/main/java/swervelib/SwerveDrive.java
@@ -511,20 +511,6 @@ public class SwerveDrive {
   }
 
   /**
-   * Gets the current module positions (azimuth and wheel position (meters)).
-   *
-   * @return A list of {@link SwerveModulePosition} containg the current module positions
-   */
-  public SwerveModulePosition[] getModulePositions() {
-    SwerveModulePosition[] positions =
-        new SwerveModulePosition[swerveDriveConfiguration.moduleCount];
-    for (SwerveModule module : swerveModules) {
-      positions[module.moduleNumber] = module.getPosition();
-    }
-    return positions;
-  }
-
-  /**
    * Set the module states (azimuth and velocity) directly.
    *
    * @param desiredStates A list of SwerveModuleStates to send to the modules.
@@ -622,8 +608,7 @@ public class SwerveDrive {
    */
   public void resetOdometry(Pose2d pose) {
     odometryLock.lock();
-    SwerveModulePosition[] positions = getModulePositions();
-    swerveDrivePoseEstimator.resetPosition(getYaw(), positions, pose);
+    swerveDrivePoseEstimator.resetPosition(getYaw(), getModulePositions(), pose);
     odometryLock.unlock();
     kinematics.toSwerveModuleStates(ChassisSpeeds.fromFieldRelativeSpeeds(0, 0, 0, getYaw()));
   }
@@ -650,6 +635,20 @@ public class SwerveDrive {
       states[module.moduleNumber] = module.getState();
     }
     return states;
+  }
+
+  /**
+   * Gets the current module positions (azimuth and wheel position (meters)).
+   *
+   * @return A list of SwerveModulePositions containg the current module positions
+   */
+  public SwerveModulePosition[] getModulePositions() {
+    SwerveModulePosition[] positions =
+        new SwerveModulePosition[swerveDriveConfiguration.moduleCount];
+    for (SwerveModule module : swerveModules) {
+      positions[module.moduleNumber] = module.getPosition();
+    }
+    return positions;
   }
 
   /**
